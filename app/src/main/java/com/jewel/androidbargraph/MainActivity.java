e1e319b85a169eeb79548972b37091a0e7e18904
nayan5565 <nayan5565@gmail.com>
2017-08-19 23:26:23 +0600

    add pichart

--- app/src/main/java/com/jewel/androidbargraph/MainActivity.java
+++ app/src/main/java/com/jewel/androidbargraph/MainActivity.java
@@ -1,108 +1,40 @@
 package com.jewel.androidbargraph;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
-
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Nayan on 8/19/2017.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnBar, btnPi;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        BarChart barChart = (BarChart) findViewById(R.id.barchart);
+        setContentView(R.layout.main_activity);
+        btnBar = (Button) findViewById(R.id.btnBar);
+        btnPi = (Button) findViewById(R.id.btnPi);
+        btnBar.setOnClickListener(this);
+        btnPi.setOnClickListener(this);
 
-        ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(8, 9));
-        entries.add(new BarEntry(0, -1));
-//        entries.add(new BarEntry(0, 2));
-//        entries.add(new BarEntry(0, 3));
-//        entries.add(new BarEntry(0, 4));
-//        entries.add(new BarEntry(0, 5));
-//        entries.add(new BarEntry(0, 6));
+    }
 
-        BarDataSet bardataset = new BarDataSet(entries, "Cells");
+    @Override
+    public void onClick(View v) {
+        if (v==btnBar){
+            Intent intent=new Intent(MainActivity.this,BarChartActivity.class);
+            startActivity(intent);
 
-        ArrayList<String> labels = new ArrayList<String>();
-        labels.add("0");
-        labels.add("1");
-        labels.add("2");
-        labels.add("3");
-        labels.add("4");
-        labels.add("5");
-        labels.add("6");
-        labels.add("7");
-        labels.add("8");
-        labels.add("9");
+        }
+        else if (v==btnPi){
+            Intent intent=new Intent(MainActivity.this,PIChartActivity.class);
+            startActivity(intent);
+        }
 
-        BarData data = new BarData(labels, bardataset);
-        barChart.setData(data); // set the data and list of lables into chart
-        barChart.setClickable(false);
-//        barChart.setX(10);
-        barChart.setDescription("Set Bar Chart Description");  // set the description
-        int[] color={
-            Color.GREEN,Color.YELLOW
-        };
-        bardataset.setColors(color);
-
-        barChart.animateY(5000);
-
-
-//        HorizontalBarChart barChart = (HorizontalBarChart) findViewById(R.id.barchart);
-//
-//        // create BarEntry for Bar Group 1
-//        ArrayList<BarEntry> bargroup1 = new ArrayList<>();
-//        bargroup1.add(new BarEntry(8f, 0));
-//        bargroup1.add(new BarEntry(2f, 1));
-//        bargroup1.add(new BarEntry(5f, 2));
-//        bargroup1.add(new BarEntry(20f, 3));
-//        bargroup1.add(new BarEntry(15f, 4));
-//        bargroup1.add(new BarEntry(19f, 5));
-//
-//        // create BarEntry for Bar Group 1
-//        ArrayList<BarEntry> bargroup2 = new ArrayList<>();
-//        bargroup2.add(new BarEntry(6f, 0));
-//        bargroup2.add(new BarEntry(10f, 1));
-//        bargroup2.add(new BarEntry(5f, 2));
-//        bargroup2.add(new BarEntry(25f, 3));
-//        bargroup2.add(new BarEntry(4f, 4));
-//        bargroup2.add(new BarEntry(17f, 5));
-//
-//        BarDataSet barDataSet1 = new BarDataSet(bargroup1, "Bar Group 1");  // creating dataset for group1
-//
-//        //barDataSet1.setColor(Color.rgb(0, 155, 0));
-//        barDataSet1.setColors(ColorTemplate.COLORFUL_COLORS);
-//
-//        BarDataSet barDataSet2 = new BarDataSet(bargroup2, "Brand 2"); // creating dataset for group1
-//        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
-//
-//        ArrayList<String> labels = new ArrayList<String>();
-//        labels.add("2016");
-//        labels.add("2015");
-//        labels.add("2014");
-//        labels.add("2013");
-//        labels.add("2012");
-//        labels.add("2011");
-//
-//        ArrayList<BarDataSet> dataSets = new ArrayList<>();  // combined all dataset into an arraylist
-//        dataSets.add(barDataSet1);
-//        dataSets.add(barDataSet2);
-//        BarData data = new BarData(labels, dataSets); // initialize the Bardata with argument labels and dataSet
-//        barChart.setData(data);
-//
-//
-//
-//
-//        barChart.setDescription("Set Bar Chart Description");  // set the description
-//
-//        barChart.animateY(5000);
     }
 }
---
+++ app/src/main/java/com/jewel/androidbargraph/PIChartActivity.java
@@ -0,0 +1,189 @@
+package com.jewel.androidbargraph;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.SeekBar;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Nayan on 8/19/2017.
+ */
+public class PIChartActivity extends AppCompatActivity implements OnChartValueSelectedListener {
+    private SeekBar seekBar, seekBar2, seekBar3, seekBar4;
+    private int w, x, y, z;
+    //    private int max;
+    //    private PieChart mPieChart;
+    private PieChart pieChart2;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pi_chart_activity);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar2 = (SeekBar) findViewById(R.id.seekBar2);
+        seekBar3 = (SeekBar) findViewById(R.id.seekBar3);
+        seekBar4 = (SeekBar) findViewById(R.id.seekBar4);
+
+
+        pieChart2 = (PieChart) findViewById(R.id.piechart2);
+        pieChart2.setOnChartValueSelectedListener(this);
+        piChart();
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                w = progress;
+//                max = seekBar.getMax() - w;
+//                piChart();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                seekBar2.setMax(max);
+                x = progress;
+//                max = seekBar2.getMax() - x;
+//                piChart();
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                seekBar3.setMax(max);
+                y = progress;
+//                max = seekBar3.getMax() - y;
+//                piChart();
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar4.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                seekBar4.setMax(max);
+                z = progress;
+                piChart();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+
+    private void piChart() {
+        // IMPORTANT: In a PieChart, no values (Entry) should have the same
+        // xIndex (even if from different DataSets), since no values can be
+        // drawn above each other.
+        ArrayList<Entry> yvalues = new ArrayList<Entry>();
+        yvalues.add(new Entry(w, 0));
+        yvalues.add(new Entry(x, 1));
+        yvalues.add(new Entry(y, 2));
+        yvalues.add(new Entry(z, 3));
+//        yvalues.add(new Entry(23f, 4));
+//        yvalues.add(new Entry(17f, 5));
+
+        PieDataSet dataSet = new PieDataSet(yvalues, "Election Results");
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        xVals.add("January");
+        xVals.add("February");
+        xVals.add("March");
+        xVals.add("April");
+        xVals.add("May");
+//        xVals.add("June");
+
+        dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+
+        PieData data = new PieData(xVals, dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        pieChart2.setUsePercentValues(true);
+        pieChart2.setData(data);
+//        pieChart2.setDrawHoleEnabled(true);
+        pieChart2.setTransparentCircleRadius(50f);
+        pieChart2.setHoleRadius(30f);
+        pieChart2.setHoleColor(Color.GRAY);
+
+        pieChart2.animateXY(1000, 1000);
+    }
+
+    @Override
+    public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
+        if (e == null)
+            return;
+        Log.e("VAL SELECTED",
+                "Value: " + e.getVal() + ", xIndex: " + e.getXIndex()
+                        + ", DataSet index: " + dataSetIndex);
+    }
+
+    @Override
+    public void onNothingSelected() {
+        Log.e("pichart", "Nothing Selected");
+
+    }
+
+//    private void piChart() {
+//        mPieChart.addPieSlice(new PieModel("Freetime", w, Color.parseColor("#FE6DA8")));
+//        mPieChart.addPieSlice(new PieModel("Sleep", x, Color.parseColor("#56B7F1")));
+//        mPieChart.addPieSlice(new PieModel("Work", y, Color.parseColor("#CDA67F")));
+//        mPieChart.addPieSlice(new PieModel("Eating", z, Color.parseColor("#FED70E")));
+//
+//
+//
+//        mPieChart.startAnimation();
+//    }
+}
